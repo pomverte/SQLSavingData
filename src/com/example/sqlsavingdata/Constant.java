046924583ceb77050af686343b5a1ede15ddc7be
PoMverte <vietnem@yahoo.fr>
2013-01-05 03:39:27 +0100

    add private constructor

--- src/com/example/sqlsavingdata/Constant.java
+++ src/com/example/sqlsavingdata/Constant.java
@@ -4,4 +4,7 @@ public abstract class Constant {
 
 	public static final String TAG_DIALOG = "com.example.sqlsavingdata";
 
+	private Constant() {
+		// Prevents the class from being instantiated.
+	}
 }
