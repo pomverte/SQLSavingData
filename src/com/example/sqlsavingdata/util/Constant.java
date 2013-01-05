8288c51a0db248dfddcd153b3718474e4070b15f
PoMverte <vietnem@yahoo.fr>
2013-01-05 18:00:53 +0100

    update tag to e more specific

--- src/com/example/sqlsavingdata/util/Constant.java
+++ src/com/example/sqlsavingdata/util/Constant.java
@@ -2,7 +2,9 @@ package com.example.sqlsavingdata.util;
 
 public abstract class Constant {
 
-	public static final String TAG_DIALOG = "com.example.sqlsavingdata";
+	public static final String TAG_DIALOG = "dialog";
+
+	public static final String TAG_SQL = "sqlite";
 
 	private Constant() {
 		// Prevents the class from being instantiated.
