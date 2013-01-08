2e50a044d20b00886bba0b2651830bd63d2f9dbd
PoMverte <vietnem@yahoo.fr>
2013-01-08 21:03:46 +0100

    features : display the users in the database on the main screen user list is refreshed after a creating a new user

--- src/com/example/sqlsavingdata/MainActivity.java
+++ src/com/example/sqlsavingdata/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.sqlsavingdata;
 
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
@@ -8,6 +9,7 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.TextView;
 
 import com.example.sqlsavingdata.sqlite.UsersEntry;
 import com.example.sqlsavingdata.sqlite.UsersProvider;
@@ -19,6 +21,7 @@ public class MainActivity extends FragmentActivity implements AddUserDialogFragm
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		fillUsersList();
 	}
 
 	@Override
@@ -52,5 +55,29 @@ public class MainActivity extends FragmentActivity implements AddUserDialogFragm
 		values.put(UsersEntry.COLUMN_NAME_USERS_EMAIL, userEmail);
 		Uri uri = getContentResolver().insert(UsersProvider.CONTENT_URI, values);
 		Log.d(Constant.TAG_SQL, uri.toString());
+		fillUsersList();
+	}
+
+	private void fillUsersList() {
+
+		Log.d(Constant.TAG_SQL, "com.example.sqlsavingdata.MainActivity.fillUsersList(View)");
+
+		String[] projection = { UsersEntry._ID, UsersEntry.COLUMN_NAME_USERS_FULLNAME, UsersEntry.COLUMN_NAME_USERS_EMAIL };
+		Cursor cursor = getContentResolver().query(UsersProvider.CONTENT_URI, projection, null, null, null);
+		int usersNumber = cursor.getCount();
+
+		TextView textView = (TextView) findViewById(R.id.users_list);
+
+		StringBuilder sb = new StringBuilder();
+		sb.append(String.format("You have %s user(s) in your database :\n", usersNumber));
+
+		while (!cursor.isAfterLast()) {
+			// fill the list with the user's name
+			String userFullName = cursor.getString(cursor.getColumnIndexOrThrow(UsersEntry.COLUMN_NAME_USERS_FULLNAME));
+			sb.append("\n\t- " + userFullName);
+			cursor.moveToNext();
+		}
+
+		textView.setText(sb.toString());
 	}
 }
